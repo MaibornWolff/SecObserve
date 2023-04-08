7fde47b1118ad734c39e5fd8808f9bdfcd627724
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2023-04-08 08:28:05 +0200

    fix: database for unittests

--- backend/config/settings/unittests.py
+++ backend/config/settings/unittests.py
@@ -1,8 +1,13 @@
 from .prod import *  # noqa
 
-# Disable SSL for database access
-DATABASES["default"]["OPTIONS"] = {}
-
+DATABASES = {
+    "default": {
+        "ATOMIC_REQUESTS": True,
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "./sqlite_unittests.db",
+        "TEST": {}
+    }
+}
 # Basically disable throttling for unit tests
 REST_FRAMEWORK["DEFAULT_THROTTLE_RATES"] = {
     "anon": "1000/second",
