19034f814aca88b6b625428aefbb56da061727d9
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2023-07-26 12:43:59 +0200

    chore: isort

--- backend/unittests/issue_tracker/issue_trackers/test_jira_issue_tracker.py
+++ backend/unittests/issue_tracker/issue_trackers/test_jira_issue_tracker.py
@@ -1,11 +1,9 @@
-from unittest.mock import patch
 from dataclasses import dataclass
+from unittest.mock import patch
 
 from application.core.models import Observation
 from application.issue_tracker.issue_trackers.base_issue_tracker import Issue
-from application.issue_tracker.issue_trackers.jira_issue_tracker import (
-    JiraIssueTracker,
-)
+from application.issue_tracker.issue_trackers.jira_issue_tracker import JiraIssueTracker
 from unittests.base_test_case import BaseTestCase
 
 
