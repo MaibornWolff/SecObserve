4a79f855809d99dcfb79a61d269cc7e80f8464a8
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2023-09-24 21:26:30 +0200

    chore: switch from react-scripts to vite (#583)

---
+++ frontend/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+  }
+});
