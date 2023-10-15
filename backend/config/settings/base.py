142b6529baa7ee0559af536c9dc10d99892077b6
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2023-10-15 18:51:29 +0200

    chore: remove unused crispy libraries

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -100,8 +100,6 @@ DJANGO_APPS = [
     "django.forms",
 ]
 THIRD_PARTY_APPS = [
-    "crispy_forms",
-    "crispy_bootstrap5",
     "rest_framework",
     "corsheaders",
     "drf_spectacular",
@@ -248,10 +246,6 @@ TEMPLATES = [
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#form-renderer
 FORM_RENDERER = "django.forms.renderers.TemplatesSetting"
-
-# http://django-crispy-forms.readthedocs.io/en/latest/install.html#template-packs
-CRISPY_TEMPLATE_PACK = "bootstrap5"
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 # FIXTURES
 # ------------------------------------------------------------------------------
