c6d1be3ec7d66c8eb05d3b9b1824ea3b71de739d
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2024-02-03 12:23:38 +0100

    fix: delete permissions for notifications

--- backend/application/commons/services/notification.py
+++ backend/application/commons/services/notification.py
@@ -1,9 +1,11 @@
 from django.db.models.query import QuerySet
 from rest_framework.exceptions import ValidationError
+from application.commons.services.global_request import get_current_user
 
 from application.commons.models import Notification
 from application.commons.queries.notification import get_notifications
-
+from application.access_control.services.authorization import user_has_permission
+from application.access_control.services.roles_permissions import Permissions
 
 def bulk_delete(notification_ids: list[int]) -> None:
     notifications = _check_notifications(notification_ids)
@@ -14,5 +16,11 @@ def _check_notifications(notification_ids: list[int]) -> QuerySet[Notification]:
     notifications = get_notifications().filter(id__in=notification_ids)
     if len(notifications) != len(notification_ids):
         raise ValidationError("Some notifications do not exist")
+
+    user = get_current_user()
+    if not user.is_superuser:
+        for notification in notifications:
+            if not notification.product or not user_has_permission(notification.product, Permissions.Product_Delete):
+                raise ValidationError("User does not have permission to delete some notifications")
 
     return notifications
