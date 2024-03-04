e65d2da89a5907b535e8444f3f01c9c49c6a57f5
dervoeti <lukas.voetmand@stackable.tech>
2024-03-04 20:11:01 +0100

    Disable debug mode / Set postgres schema

--- backend/config/settings/dist.py
+++ backend/config/settings/dist.py
@@ -4,7 +4,7 @@ from .base import env
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = True
+DEBUG = False
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env(
     "DJANGO_SECRET_KEY",
