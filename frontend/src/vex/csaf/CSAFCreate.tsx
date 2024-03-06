3c7d09d5bc7886a5461d0c383559540ed9b1dc4c
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2024-03-06 19:55:04 +0100

    chore: prettier

--- frontend/src/vex/csaf/CSAFCreate.tsx
+++ frontend/src/vex/csaf/CSAFCreate.tsx
@@ -16,11 +16,7 @@ import {
 } from "react-admin";
 
 import axios_instance from "../../access_control/axios_instance";
-import {
-    validate_255,
-    validate_required,
-    validate_required_255,
-} from "../../commons/custom_validators";
+import { validate_255, validate_required, validate_required_255 } from "../../commons/custom_validators";
 import { AutocompleteInputMedium, AutocompleteInputWide, TextInputWide } from "../../commons/layout/themes";
 import { CSAF_PUBLISHER_CATEGORY_CHOICES, CSAF_TLP_LABEL_CHOICES, CSAF_TRACKING_STATUS_CHOICES } from "../types";
 
