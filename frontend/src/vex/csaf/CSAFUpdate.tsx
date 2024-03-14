3d2ad485b7035748e3e876fd2b22a4f07f70731e
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2024-03-14 21:19:10 +0100

    chore: progress spinners for vex dialogs (#1232)

--- frontend/src/vex/csaf/CSAFUpdate.tsx
+++ frontend/src/vex/csaf/CSAFUpdate.tsx
@@ -1,6 +1,15 @@
 import CancelIcon from "@mui/icons-material/Cancel";
 import EditIcon from "@mui/icons-material/Edit";
-import { Button, Dialog, DialogContent, DialogTitle, Divider, Typography } from "@mui/material";
+import {
+    Backdrop,
+    Button,
+    CircularProgress,
+    Dialog,
+    DialogContent,
+    DialogTitle,
+    Divider,
+    Typography,
+} from "@mui/material";
 import { Fragment, useState } from "react";
 import { SaveButton, SimpleForm, Toolbar, useNotify, useRefresh } from "react-admin";
 
@@ -11,13 +20,18 @@ import { CSAF_PUBLISHER_CATEGORY_CHOICES, CSAF_TLP_LABEL_CHOICES, CSAF_TRACKING_
 
 const CSAFUpdate = () => {
     const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
     const refresh = useRefresh();
     const notify = useNotify();
     const handleOpen = () => setOpen(true);
-    const handleCancel = () => setOpen(false);
+    const handleCancel = () => {
+        setOpen(false);
+        setLoading(false);
+    };
     const handleClose = (event: object, reason: string) => {
         if (reason && reason == "backdropClick") return;
         setOpen(false);
+        setLoading(false);
     };
 
     const CancelButton = () => (
@@ -45,11 +59,14 @@ const CSAFUpdate = () => {
     );
 
     const update_csaf = async (data: any) => {
+        setLoading(true);
+
         const url = "vex/csaf_document/update/" + data.document_id_prefix + "/" + data.document_base_id + "/";
         axios_instance
             .post(url, data, { responseType: "blob" })
             .then(function (response) {
                 if (response.status == 204) {
+                    setLoading(false);
                     notify("No changes in CSAF document", {
                         type: "warning",
                     });
@@ -62,6 +79,7 @@ const CSAFUpdate = () => {
                     link.click();
 
                     refresh();
+                    setLoading(false);
                     notify("CASF document updated", {
                         type: "success",
                     });
@@ -69,6 +87,7 @@ const CSAFUpdate = () => {
                 setOpen(false);
             })
             .catch(async function (error) {
+                setLoading(false);
                 notify(await error.response.data.text(), {
                     type: "warning",
                 });
@@ -85,7 +104,7 @@ const CSAFUpdate = () => {
             >
                 Update CSAF document
             </Button>
-            <Dialog open={open} onClose={handleClose} maxWidth={"lg"}>
+            <Dialog open={open && !loading} onClose={handleClose} maxWidth={"lg"}>
                 <DialogTitle>Update CSAF document</DialogTitle>
                 <DialogContent>
                     <SimpleForm onSubmit={update_csaf} toolbar={<CustomToolbar />}>
@@ -113,6 +132,11 @@ const CSAFUpdate = () => {
                     </SimpleForm>
                 </DialogContent>
             </Dialog>
+            {loading ? (
+                <Backdrop sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }} open={open}>
+                    <CircularProgress color="primary" />
+                </Backdrop>
+            ) : null}
         </Fragment>
     );
 };
