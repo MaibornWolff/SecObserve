9a01e27048ab9601bc61e15b0c54b4d5d8416e97
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2024-03-24 08:14:23 +0100

    chore: black

--- backend/unittests/core/services/test_observation_log.py
+++ backend/unittests/core/services/test_observation_log.py
@@ -20,7 +20,12 @@ class TestObservationLog(BaseTestCase):
         mock_user.return_value = self.user_internal
 
         observation_log = create_observation_log(
-            self.observation_1, "severity", "status", "comment", "vex_justification", Assessment_Status.ASSESSMENT_STATUS_AUTO_APPROVED
+            self.observation_1,
+            "severity",
+            "status",
+            "comment",
+            "vex_justification",
+            Assessment_Status.ASSESSMENT_STATUS_AUTO_APPROVED,
         )
 
         self.assertEqual(self.observation_1, observation_log.observation)
@@ -29,7 +34,10 @@ class TestObservationLog(BaseTestCase):
         self.assertEqual("status", observation_log.status)
         self.assertEqual("comment", observation_log.comment)
         self.assertEqual("vex_justification", observation_log.vex_justification)
-        self.assertEqual(Assessment_Status.ASSESSMENT_STATUS_AUTO_APPROVED, observation_log.assessment_status)
+        self.assertEqual(
+            Assessment_Status.ASSESSMENT_STATUS_AUTO_APPROVED,
+            observation_log.assessment_status,
+        )
 
         self.assertEqual(
             self.observation_1.last_observation_log, observation_log.created
