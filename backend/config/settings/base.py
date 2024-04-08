bd8c77a0afd0017b695aa061b976c9751ee75830
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2024-04-08 18:54:04 +0200

    fix: docker healthceck wasn't working (#1350)

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -25,7 +25,7 @@ DEBUG = env.bool("DJANGO_DEBUG", False)
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", env("ALLOWED_HOSTS")]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", env("ALLOWED_HOSTS")]
 
 # Local time zone. Choices are
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
