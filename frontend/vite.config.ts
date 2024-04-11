3de49edd4288f0666c7531546c637c033e29437b
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2024-04-11 09:31:38 +0200

    chore: refactor loading of password rules (#1365)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    supported: {
-      'top-level-await': true //browsers can handle top-level-await features
-    }
-  },
   build: {
     outDir: 'build',
   },
