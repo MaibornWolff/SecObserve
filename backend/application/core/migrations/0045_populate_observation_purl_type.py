e41c3a3a43be7b5ee69184f5d3f36c284195e516
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2024-08-16 09:01:23 +0200

    chore: population of purl_type with pagination

--- backend/application/core/migrations/0045_populate_observation_purl_type.py
+++ backend/application/core/migrations/0045_populate_observation_purl_type.py
@@ -1,5 +1,6 @@
 import logging
 
+from django.core.paginator import Paginator
 from django.db import migrations
 
 from application.core.services.observation import normalize_origin_component
@@ -9,11 +10,22 @@ logger = logging.getLogger("secobserve.migration")
 
 def populate_purl_type(apps, schema_editor):
     Observation = apps.get_model("core", "Observation")
-    for observation in Observation.objects.exclude(origin_component_purl="").filter(
-        origin_component_purl_type=""
-    ):
-        normalize_origin_component(observation)
-        observation.save()
+    observations = (
+        Observation.objects.exclude(origin_component_purl="")
+        .filter(origin_component_purl_type="")
+        .order_by("id")
+    )
+
+    paginator = Paginator(observations, 1000)
+    for page_number in paginator.page_range:
+        page = paginator.page(page_number)
+        updates = []
+
+        for observation in page.object_list:
+            normalize_origin_component(observation)
+            updates.append(observation)
+
+        Observation.objects.bulk_update(updates, ["origin_component_purl_type"])
 
 
 class Migration(migrations.Migration):
