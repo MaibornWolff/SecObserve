ffb6d7b1fbb151fe4384d3931b287f6725f3da0f
Julian Volland <julian.volland@datev.de>
2024-08-20 15:20:43 +0200

    added create_observation method to fix pylint R0914

--- backend/application/import_observations/parsers/trivy_prometheus/parser.py
+++ backend/application/import_observations/parsers/trivy_prometheus/parser.py
@@ -80,68 +80,73 @@ class TrivyPrometheus(BaseParser, BaseAPIParser):
 
         return True, [], data
 
+    def __create_observation(self, finding) -> Observation:
+        origin_component_name = finding.get("metric", {}).get("resource", "")
+        vuln_title = finding.get("metric", {}).get("vuln_title", "")
+        vulnerability_id = finding.get("metric", {}).get("vuln_id", "")
+        cvss3_score = finding.get("metric", {}).get("vuln_score")
+        severity = finding.get("metric", {}).get(
+            "severity", Severity.SEVERITY_UNKOWN
+        )
+        origin_docker_image_registry = finding.get("metric", {}).get(
+            "image_registry", ""
+        )
+        origin_docker_image_repository = finding.get("metric", {}).get(
+            "image_repository", ""
+        )
+        origin_docker_image_name = (
+            origin_docker_image_registry + "/" + origin_docker_image_repository
+        )
+        origin_docker_image_tag = finding.get("metric", {}).get("image_tag", "")
+        fixed_version = finding.get("metric", {}).get("fixed_version", "")
+        origin_component_version = finding.get("metric", {}).get(
+            "installed_version", ""
+        )
+        namespace = finding.get("metric", {}).get("namespace", "")
+        resource_kind = finding.get("metric", {}).get("resource_kind", "")
+        resource_name = finding.get("metric", {}).get("resource_name", "")
+        container_name = finding.get("metric", {}).get("container_name", "")
+        prometheus_endpoint_url = (
+            self.api_configuration.base_url
+            if isinstance(self.api_configuration, Api_Configuration)
+            else ""
+        )
+
+        observation = Observation(
+            title=vulnerability_id,
+            parser_severity=self.get_severity(severity),
+            numerical_severity=cvss3_score,
+            vulnerability_id=vulnerability_id,
+            origin_docker_image_name=origin_docker_image_name,
+            origin_docker_image_tag=origin_docker_image_tag,
+            cvss3_score=cvss3_score,
+            origin_component_name=origin_component_name,
+            scanner="Trivy Prometheus",
+            origin_component_version=origin_component_version,
+            recommendation=self.get_recommendation(
+                fixed_version, origin_component_version
+            ),
+            description=self.get_description(
+                vuln_title=vuln_title,
+                prometheus_endpoint_url=prometheus_endpoint_url,
+                kubernetes_resource={
+                    "namespace": namespace,
+                    "resource_kind": resource_kind,
+                    "resource_name": resource_name,
+                    "container_name": container_name
+                },
+            ),
+        )
+
+        return observation
+
     def get_observations(self, data) -> list[Observation]:
         observations = []
 
         for finding in data.get("data").get("result"):
             if not finding.get("metric", {}).get("vuln_id", ""):
                 continue
-            origin_component_name = finding.get("metric", {}).get("resource", "")
-            vuln_title = finding.get("metric", {}).get("vuln_title", "")
-            vulnerability_id = finding.get("metric", {}).get("vuln_id", "")
-            cvss3_score = finding.get("metric", {}).get("vuln_score")
-            severity = finding.get("metric", {}).get(
-                "severity", Severity.SEVERITY_UNKOWN
-            )
-            origin_docker_image_registry = finding.get("metric", {}).get(
-                "image_registry", ""
-            )
-            origin_docker_image_repository = finding.get("metric", {}).get(
-                "image_repository", ""
-            )
-            origin_docker_image_name = (
-                origin_docker_image_registry + "/" + origin_docker_image_repository
-            )
-            origin_docker_image_tag = finding.get("metric", {}).get("image_tag", "")
-            fixed_version = finding.get("metric", {}).get("fixed_version", "")
-            origin_component_version = finding.get("metric", {}).get(
-                "installed_version", ""
-            )
-            namespace = finding.get("metric", {}).get("namespace", "")
-            resource_kind = finding.get("metric", {}).get("resource_kind", "")
-            resource_name = finding.get("metric", {}).get("resource_name", "")
-            container_name = finding.get("metric", {}).get("container_name", "")
-            prometheus_endpoint_url = (
-                self.api_configuration.base_url
-                if isinstance(self.api_configuration, Api_Configuration)
-                else ""
-            )
-
-            observation = Observation(
-                title=vulnerability_id,
-                parser_severity=self.get_severity(severity),
-                numerical_severity=cvss3_score,
-                vulnerability_id=vulnerability_id,
-                origin_docker_image_name=origin_docker_image_name,
-                origin_docker_image_tag=origin_docker_image_tag,
-                cvss3_score=cvss3_score,
-                origin_component_name=origin_component_name,
-                scanner="Trivy Prometheus",
-                origin_component_version=origin_component_version,
-                recommendation=self.get_recommendation(
-                    fixed_version, origin_component_version
-                ),
-                description=self.get_description(
-                    vuln_title=vuln_title,
-                    prometheus_endpoint_url=prometheus_endpoint_url,
-                    kubernetes_resource={
-                        "namespace": namespace,
-                        "resource_kind": resource_kind,
-                        "resource_name": resource_name,
-                        "container_name": container_name
-                    },
-                ),
-            )
+            observation = self.__create_observation(finding)
 
             evidence = []
             evidence.append("Vulnerability")
