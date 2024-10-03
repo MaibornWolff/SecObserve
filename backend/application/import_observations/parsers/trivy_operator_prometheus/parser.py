0661c5b4795f693331b2a3f92a52c7bf5d730b1a
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2024-10-03 15:40:50 +0200

    feat: more scans for Trivy Operator

--- backend/application/import_observations/parsers/trivy_operator_prometheus/parser.py
+++ backend/application/import_observations/parsers/trivy_operator_prometheus/parser.py
@@ -1,4 +1,5 @@
 import json
+from dataclasses import dataclass
 from typing import Optional
 
 import requests
@@ -11,6 +12,13 @@ from application.import_observations.parsers.base_parser import (
     BaseParser,
 )
 from application.import_observations.types import Parser_Type
+
+
+@dataclass
+class KubernetesResource:
+    namespace: str
+    resource_kind: str
+    resource_name: str
 
 
 class TrivyOperatorPrometheus(BaseParser, BaseAPIParser):
@@ -86,7 +94,136 @@ class TrivyOperatorPrometheus(BaseParser, BaseAPIParser):
 
         return True, [], data
 
-    def __create_observation(self, finding) -> Observation:
+    def get_observations(self, data: dict) -> list[Observation]:
+        observations = []
+        for finding in data.get("data", {}).get("result"):
+            if (
+                finding.get("metric", {}).get("__name__", "") == "trivy_compliance_info"
+                and finding.get("metric", {}).get("status", "") == "Fail"
+            ):
+                observation = self._create_compliance_observation(finding)
+            elif (
+                finding.get("metric", {}).get("__name__", "")
+                == "trivy_configaudits_info"
+                and finding.get("metric", {}).get("config_audit_success", "") == "false"
+            ):
+                observation = self._create_config_audit_observation(finding)
+            elif (
+                finding.get("metric", {}).get("__name__", "")
+                == "trivy_exposedsecrets_info"
+            ):
+                observation = self._create_exposedsecrets_observation(finding)
+            elif (
+                finding.get("metric", {}).get("__name__", "")
+                == "trivy_rbacassessments_info"
+                and finding.get("metric", {}).get("rbac_assessment_success", "")
+                == "false"
+            ):
+                observation = self._create_rbac_assessment_observation(finding)
+            elif (
+                finding.get("metric", {}).get("__name__", "")
+                == "trivy_vulnerability_id"
+            ):
+                observation = self._create_vulnerability_observation(finding)
+            else:
+                continue
+
+            kubernetes_resource = self._get_kubernetes_resource(finding)
+            observation.origin_kubernetes_namespace = kubernetes_resource.namespace
+            observation.origin_kubernetes_resource_type = (
+                kubernetes_resource.resource_kind
+            )
+
+            if (
+                finding.get("metric", {}).get("__name__", "")
+                == "trivy_rbacassessments_info"
+                and not kubernetes_resource.resource_name
+            ):
+                kubernetes_resource.resource_name = finding.get("metric", {}).get(
+                    "name", ""
+                )
+
+            observation.origin_kubernetes_resource_name = (
+                kubernetes_resource.resource_name
+            )
+
+            observation.scanner = "Trivy Operator"
+
+            evidence = []
+            evidence.append("Vulnerability")
+            evidence.append(json.dumps(finding))
+            observation.unsaved_evidences.append(evidence)
+
+            observations.append(observation)
+
+        return observations
+
+    def _create_compliance_observation(self, finding) -> Observation:
+        title = finding.get("metric", {}).get("title", "")
+        compliance_name = finding.get("metric", {}).get("compliance_name", "")
+        compliance_id = finding.get("metric", {}).get("compliance_id", "")
+        description = finding.get("metric", {}).get("description", "")
+        severity = finding.get("metric", {}).get("severity", Severity.SEVERITY_UNKOWN)
+
+        return Observation(
+            title=f"{title} / {compliance_name}",
+            parser_severity=self._get_severity(severity),
+            description=self._get_description(
+                description=description, audit_id=compliance_id
+            ),
+        )
+
+    def _create_config_audit_observation(self, finding) -> Observation:
+        config_audit_title = finding.get("metric", {}).get("config_audit_title", "")
+        config_audit_description = finding.get("metric", {}).get(
+            "config_audit_description", ""
+        )
+        config_audit_id = finding.get("metric", {}).get("config_audit_id", "")
+        severity = finding.get("metric", {}).get("severity", Severity.SEVERITY_UNKOWN)
+
+        return Observation(
+            title=config_audit_title,
+            parser_severity=self._get_severity(severity),
+            description=self._get_description(
+                description=config_audit_description, audit_id=config_audit_id
+            ),
+        )
+
+    def _create_exposedsecrets_observation(self, finding) -> Observation:
+        image_registry = finding.get("metric", {}).get("image_registry", "")
+        image_repository = finding.get("metric", {}).get("image_repository", "")
+        image_tag = finding.get("metric", {}).get("image_tag", "")
+        secret_target = finding.get("metric", {}).get("secret_target", "")
+        secret_title = finding.get("metric", {}).get("secret_title", "")
+        severity = finding.get("metric", {}).get("severity", Severity.SEVERITY_UNKOWN)
+
+        return Observation(
+            title=secret_title,
+            parser_severity=self._get_severity(severity),
+            origin_docker_image_name=f"{image_registry}/{image_repository}",
+            origin_docker_image_tag=image_tag,
+            origin_source_file=secret_target,
+        )
+
+    def _create_rbac_assessment_observation(self, finding) -> Observation:
+        rbac_assessment_title = finding.get("metric", {}).get(
+            "rbac_assessment_title", ""
+        )
+        rbac_assessment_description = finding.get("metric", {}).get(
+            "rbac_assessment_description", ""
+        )
+        rbac_assessment_id = finding.get("metric", {}).get("rbac_assessment_id", "")
+        severity = finding.get("metric", {}).get("severity", Severity.SEVERITY_UNKOWN)
+
+        return Observation(
+            title=rbac_assessment_title,
+            parser_severity=self._get_severity(severity),
+            description=self._get_description(
+                description=rbac_assessment_description, audit_id=rbac_assessment_id
+            ),
+        )
+
+    def _create_vulnerability_observation(self, finding) -> Observation:
         origin_component_name = finding.get("metric", {}).get("resource", "")
         vuln_title = finding.get("metric", {}).get("vuln_title", "")
         vulnerability_id = finding.get("metric", {}).get("vuln_id", "")
@@ -106,69 +243,24 @@ class TrivyOperatorPrometheus(BaseParser, BaseAPIParser):
         origin_component_version = finding.get("metric", {}).get(
             "installed_version", ""
         )
-        namespace = finding.get("metric", {}).get("namespace", "")
-        resource_kind = finding.get("metric", {}).get("resource_kind", "")
-        resource_name = finding.get("metric", {}).get("resource_name", "")
-        prometheus_endpoint_url = (
-            self.api_configuration.base_url
-            if isinstance(self.api_configuration, Api_Configuration)
-            else ""
-        )
 
-        observation = Observation(
+        return Observation(
             title=vulnerability_id,
-            parser_severity=self.get_severity(severity),
+            parser_severity=self._get_severity(severity),
             numerical_severity=cvss3_score,
             vulnerability_id=vulnerability_id,
             origin_docker_image_name=origin_docker_image_name,
             origin_docker_image_tag=origin_docker_image_tag,
             cvss3_score=cvss3_score,
             origin_component_name=origin_component_name,
-            scanner="Trivy Operator",
             origin_component_version=origin_component_version,
-            origin_kubernetes_namespace=namespace,
-            origin_kubernetes_resource_type=resource_kind,
-            origin_kubernetes_resource_name=resource_name,
-            recommendation=self.get_recommendation(
+            recommendation=self._get_vulnerability_recommendation(
                 fixed_version, origin_component_version
             ),
-            description=self.get_description(
-                vuln_title=vuln_title,
-                prometheus_endpoint_url=prometheus_endpoint_url,
-            ),
+            description=self._get_description(description=vuln_title),
         )
 
-        return observation
-
-    def get_observations(self, data) -> list[Observation]:
-        observations = []
-
-        for finding in data.get("data").get("result"):
-            if not finding.get("metric", {}).get("vuln_id", ""):
-                continue
-            observation = self.__create_observation(finding)
-
-            evidence = []
-            evidence.append("Vulnerability")
-            evidence.append(json.dumps(finding))
-            observation.unsaved_evidences.append(evidence)
-
-            observations.append(observation)
-
-        return observations
-
-    def get_description(
-        self,
-        vuln_title,
-        prometheus_endpoint_url,
-    ) -> str:
-        description = vuln_title
-        if prometheus_endpoint_url:
-            description += f"\n\n**Prometheus host:** {prometheus_endpoint_url}"
-
-        return description
-
-    def get_recommendation(
+    def _get_vulnerability_recommendation(
         self,
         fixed_version,
         origin_component_version,
@@ -181,7 +273,31 @@ class TrivyOperatorPrometheus(BaseParser, BaseAPIParser):
 
         return recommendation
 
-    def get_severity(self, severity: str) -> str:
+    def _get_description(
+        self,
+        description: str,
+        audit_id: Optional[str] = None,
+    ) -> str:
+        if audit_id:
+            if audit_id.upper().startswith("KSV"):
+                description += (
+                    f"\n\n**Assessment ID:** [{audit_id}]"
+                    + f"(https://avd.aquasec.com/misconfig/kubernetes/{audit_id.lower()})"
+                )
+            else:
+                description += f"\n\n**Assessment ID:** {audit_id}"
+
+        prometheus_endpoint_url = (
+            self.api_configuration.base_url
+            if isinstance(self.api_configuration, Api_Configuration)
+            else ""
+        )
+        if prometheus_endpoint_url:
+            description += f"\n\n**Prometheus host:** {prometheus_endpoint_url}"
+
+        return description
+
+    def _get_severity(self, severity: str) -> str:
         if (
             severity.capitalize(),
             severity.capitalize(),
@@ -189,3 +305,10 @@ class TrivyOperatorPrometheus(BaseParser, BaseAPIParser):
             return severity.capitalize()
 
         return Severity.SEVERITY_UNKOWN
+
+    def _get_kubernetes_resource(self, finding) -> KubernetesResource:
+        return KubernetesResource(
+            namespace=finding.get("metric", {}).get("namespace", ""),
+            resource_kind=finding.get("metric", {}).get("resource_kind", ""),
+            resource_name=finding.get("metric", {}).get("resource_name", ""),
+        )
