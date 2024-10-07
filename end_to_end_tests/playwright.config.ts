192117607d3f52badacef4373f33fa3d43ba7219
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2024-10-07 10:16:57 +0200

    fix: set workers to 3

--- end_to_end_tests/playwright.config.ts
+++ end_to_end_tests/playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  // workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html', { open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -30,6 +30,7 @@ export default defineConfig({
     trace: 'on-first-retry',
   },
 
+  workers: 3,
   timeout: 60000,
     
   /* Configure projects for major browsers */
