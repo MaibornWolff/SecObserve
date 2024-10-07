0d1047a07fb724dc0f6fd9f73ceb1daaabb13ac8
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2024-10-07 10:08:29 +0200

    chore: reactivate end-2-end tests

--- end_to_end_tests/playwright.config.ts
+++ end_to_end_tests/playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     trace: 'on-first-retry',
   },
 
-  timeout: 20000,
+  timeout: 60000,
     
   /* Configure projects for major browsers */
   projects: [
@@ -38,16 +38,14 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
     /* Test against mobile viewports. */
     // {
