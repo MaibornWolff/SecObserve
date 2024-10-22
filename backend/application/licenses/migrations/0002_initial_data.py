cc495ab6490a22fa9cf8cf1519e90b1269838d15
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2024-10-22 09:58:38 +0200

    chore: move nosemgrep in migration

--- backend/application/licenses/migrations/0002_initial_data.py
+++ backend/application/licenses/migrations/0002_initial_data.py
@@ -6,8 +6,8 @@ def load_initial_data(apps, schema_editor):
     call_command("loaddata", "application/licenses/migrations/data/initial_data.json")
 
 
-def reverse(apps, schema_editor):
-    pass  # nosemgrep
+def reverse(apps, schema_editor):  # nosemgrep
+    pass
     # This is a no-op on purpose because we can't reverse loading initial data
 
 
