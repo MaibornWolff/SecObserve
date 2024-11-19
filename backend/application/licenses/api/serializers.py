9c9d9ad72696102d6bda8a5975791cd3a7238157
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2024-11-19 19:50:35 +0100

    chore: mypy

--- backend/application/licenses/api/serializers.py
+++ backend/application/licenses/api/serializers.py
@@ -96,7 +96,7 @@ class LicenseComponentSerializer(ModelSerializer):
     branch_name = SerializerMethodField()
     license_policy_name: Optional[SerializerMethodField] = SerializerMethodField()
     license_policy_id: Optional[SerializerMethodField] = SerializerMethodField()
-    evidences = NestedLicenseComponentEvidenceSerializer(many=True)
+    evidencesOptional: Optional[NestedLicenseComponentEvidenceSerializer] = NestedLicenseComponentEvidenceSerializer(many=True)
     title = SerializerMethodField()
 
     class Meta:
