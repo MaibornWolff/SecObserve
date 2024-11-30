f165a584eb431176b861a09ad20190a7456a9c68
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2024-11-30 21:38:00 +0100

    fix: prev/next button for license component show coming from overview list

--- frontend/src/licenses/license_components/LicenseComponentShow.tsx
+++ frontend/src/licenses/license_components/LicenseComponentShow.tsx
@@ -19,15 +19,36 @@ import MermaidDependencies from "../../core/observations/Mermaid_Dependencies";
 import LicenseComponentShowAside from "./LicenseComponentShowAside";
 
 const ShowActions = () => {
-    const component = useRecordContext();
+    const license_component = useRecordContext();
+
+    const filter = () => {
+        // eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style
+        const filter: { [key: string]: any } = {};
+        if (license_component) {
+            filter["product"] = Number(license_component.product);
+        }
+        const license_component_expand_filters = localStorage.getItem("license_component_expand_filters");
+        const storedFilters = license_component_expand_filters ? JSON.parse(license_component_expand_filters) : {};
+        if (storedFilters.storedFilters) {
+            if (storedFilters.storedFilters.branch_name) {
+                filter["branch_name"] = storedFilters.storedFilters.branch_name;
+            }
+            if (storedFilters.storedFilters.license_name) {
+                filter["license_name_exact"] = storedFilters.storedFilters.license_name;
+            }
+            if (storedFilters.storedFilters.evaluation_result) {
+                filter["evaluation_result"] = storedFilters.storedFilters.evaluation_result;
+            }
+        }
+        return filter;
+    };
+
     return (
         <TopToolbar>
             <Stack direction="row" justifyContent="space-between" alignItems="center">
-                {component && (
+                {license_component && (
                     <PrevNextButtons
-                        filter={{
-                            product: Number(component.product),
-                        }}
+                        filter={filter()}
                         queryOptions={{ meta: { api_resource: "license_component_ids" } }}
                         linkType="show"
                         sort={{ field: "evaluation_result", order: "ASC" }}
