6f93503f10c16814908af461bafe29fc469675b0
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2025-02-09 18:04:20 +0000

    chore: unittests for osv scanning (#2560)
    * chore: unittests for osv scanning
    
    * chore: black and isort
    
    * chore: refactor osv_parser
    
    * chore: unittests for osv parser
    
    * chore: remove experimental flag in docs

--- backend/application/import_observations/types.py
+++ backend/application/import_observations/types.py
@@ -1,5 +1,6 @@
-from dataclasses import dataclass
-from datetime import datetime
+from typing import Optional
+
+from semver import Version
 
 
 class Parser_Source:
@@ -45,14 +46,129 @@ class Parser_Filetype:
     FILETYPE_JSON = "JSON"
 
 
-@dataclass(frozen=True)
-class OSV_Vulnerability:
-    id: str
-    modified: datetime
+class ExtendedSemVer:
+    def __init__(self):
+        self.prefix: Optional[int] = None
+        self.semver: Version
 
+    @classmethod
+    def parse(cls, version: Optional[str]) -> Optional["ExtendedSemVer"]:
+        if not version:
+            return None
 
-@dataclass
-class OSV_Component:
-    id: int
-    component_purl: str
-    vulnerabilities: set[OSV_Vulnerability]
+        instance = cls()
+
+        elements = version.split(":")
+        if len(elements) >= 2:
+            prefix = elements[0]
+            if not prefix.isdigit():
+                return None
+            instance.prefix = int(prefix)
+
+            suffix = ":".join(elements[1:])
+        else:
+            suffix = version
+
+        suffix_semver = instance._get_semver(suffix)
+        if not suffix_semver:
+            return None
+        instance.semver = suffix_semver
+
+        return instance
+
+    @classmethod
+    def _get_semver(cls, suffix: str) -> Optional[Version]:
+        if not suffix:
+            return None
+
+        # Go packages sometimes have a "v" prefix
+        if suffix.startswith("v"):
+            suffix = suffix[1:]
+
+        if suffix == "0":
+            return Version.parse("0.0.0")
+
+        if len(suffix.split("-")) == 2:
+            prefix = suffix.split("-")[0]
+            if len(prefix.split(".")) == 2:
+                prefix = f"{prefix}.0"
+            suffix = f"{prefix}-{suffix.split('-')[1]}"
+
+        if len(suffix.split(".")) == 2:
+            suffix = f"{suffix}.0"
+
+        if not Version.is_valid(suffix):
+            return None
+
+        return Version.parse(suffix)
+
+    def __eq__(self, other) -> bool:
+        return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+
+    def __gt__(self, other) -> bool:
+        if not isinstance(other, self.__class__):
+            return False
+
+        if (self.prefix is None and other.prefix is not None) or (
+            self.prefix is not None and other.prefix is None
+        ):
+            return False
+
+        if self.prefix is not None and other.prefix is not None:
+            if self.prefix < other.prefix:
+                return False
+            if self.prefix > other.prefix:
+                return True
+
+        return self.semver > other.semver
+
+    def __ge__(self, other) -> bool:
+        if not isinstance(other, self.__class__):
+            return False
+
+        if (self.prefix is None and other.prefix is not None) or (
+            self.prefix is not None and other.prefix is None
+        ):
+            return False
+
+        if self.prefix is not None and other.prefix is not None:
+            if self.prefix < other.prefix:
+                return False
+            if self.prefix > other.prefix:
+                return True
+
+        return self.semver >= other.semver
+
+    def __lt__(self, other) -> bool:
+        if not isinstance(other, self.__class__):
+            return False
+
+        if (self.prefix is None and other.prefix is not None) or (
+            self.prefix is not None and other.prefix is None
+        ):
+            return False
+
+        if self.prefix is not None and other.prefix is not None:
+            if self.prefix > other.prefix:
+                return False
+            if self.prefix < other.prefix:
+                return True
+
+        return self.semver < other.semver
+
+    def __le__(self, other) -> bool:
+        if not isinstance(other, self.__class__):
+            return False
+
+        if (self.prefix is None and other.prefix is not None) or (
+            self.prefix is not None and other.prefix is None
+        ):
+            return False
+
+        if self.prefix is not None and other.prefix is not None:
+            if self.prefix > other.prefix:
+                return False
+            if self.prefix < other.prefix:
+                return True
+
+        return self.semver <= other.semver
