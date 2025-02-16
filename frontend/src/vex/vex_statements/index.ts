44d179b753e2d242b9acc71f129b708e203c2da1
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2025-02-16 19:09:04 +0100

    feat: more modern dialogue for vex imports

--- frontend/src/vex/vex_statements/index.ts
+++ frontend/src/vex/vex_statements/index.ts
@@ -1,5 +1,8 @@
+import VEXStatementIcon from "@mui/icons-material/Security";
+
 import VEXStatementShow from "./VEXStatementShow";
 
 export default {
     show: VEXStatementShow,
+    icon: VEXStatementIcon,
 };
