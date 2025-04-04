1a478bf5507d2020c3fbd99a67689e972ec8ae55
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2025-04-04 19:30:11 +0200

    chore: remove SonarQube security hotspots

--- backend/application/commons/views.py
+++ backend/application/commons/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpRequest, HttpResponse
+from django.views.decorators.http import require_GET
 
 
+@require_GET
 def empty_view(request: HttpRequest) -> HttpResponse:
     return HttpResponse(status=204)
--- backend/config/settings/dist.py
+++ backend/config/settings/dist.py
@@ -1,4 +1,4 @@
-from .base import *  # noqa
+from .base import *  # noqa NOSONAR
 from .base import env
 
 # GENERAL
@@ -27,26 +27,6 @@ EMAIL_PORT = 1025
 # ------------------------------------------------------------------------------
 # http://whitenoise.evans.io/en/latest/django.html#using-whitenoise-in-development
 INSTALLED_APPS = ["whitenoise.runserver_nostatic"] + INSTALLED_APPS  # noqa F405
-
-
-# django-debug-toolbar
-# ------------------------------------------------------------------------------
-# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
-# INSTALLED_APPS += ["debug_toolbar"]  # noqa F405
-# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
-# MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
-# https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
-# DEBUG_TOOLBAR_CONFIG = {
-#     "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
-#     "SHOW_TEMPLATE_CONTEXT": True,
-# }
-# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
-INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
-if env("USE_DOCKER") == "yes":
-    import socket
-
-    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-    INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
 
 # django-extensions
 # ------------------------------------------------------------------------------
