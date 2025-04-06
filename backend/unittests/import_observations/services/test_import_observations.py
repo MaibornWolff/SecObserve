b15fff8b96ed5808cb0b2b4fe17fab3264967180
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2025-04-06 20:47:35 +0200

    chore: prepare for release 1.30.0

--- backend/unittests/import_observations/services/test_import_observations.py
+++ backend/unittests/import_observations/services/test_import_observations.py
@@ -474,7 +474,7 @@ class TestImportObservations(BaseTestCase):
             )
             self.assertEqual(license_components[1].component_purl_type, "pypi")
             self.assertEqual(license_components[1].component_cpe, "")
-            dependencies = """SecObserve:1.29.2 --> argon2-cffi:23.1.0
+            dependencies = """SecObserve:1.30.0 --> argon2-cffi:23.1.0
 argon2-cffi:23.1.0 --> argon2-cffi-bindings:21.2.0"""
             self.assertEqual(license_components[1].component_dependencies, dependencies)
             self.assertEqual(license_components[1].license, License.objects.get(spdx_id="MIT"))
