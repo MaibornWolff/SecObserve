8d44cf045d8a794e41d57620da1a619c55725e30
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2025-07-20 11:39:35 +0200

    chore: black and isort

--- backend/unittests/background_tasks/services/test_task_base.py
+++ backend/unittests/background_tasks/services/test_task_base.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch, MagicMock, call
+from unittest.mock import MagicMock, call, patch
 
 from application.background_tasks.services.task_base import (
     handle_periodic_task_exception,
@@ -19,16 +19,15 @@ class TestTaskBase(BaseTestCase):
         mock_lock_task.return_value = lambda func: func
         test_function = MagicMock()
         test_function.__name__ = "test_function"
-        
+
         # Execute
         decorated_function = so_periodic_task("test_task")(test_function)
         decorated_function()
-        
+
         # Assert
-        mock_logger.info.assert_has_calls([
-            call("--- %s - start ---", "test_task"),
-            call("--- %s - finished ---", "test_task")
-        ])
+        mock_logger.info.assert_has_calls(
+            [call("--- %s - start ---", "test_task"), call("--- %s - finished ---", "test_task")]
+        )
         test_function.assert_called_once()
         mock_lock_task.assert_called_once_with("test_task")
 
@@ -41,11 +40,11 @@ class TestTaskBase(BaseTestCase):
         test_exception = Exception("Test exception")
         test_function = MagicMock(side_effect=test_exception)
         test_function.__name__ = "test_function"
-        
+
         # Execute
         decorated_function = so_periodic_task("test_task")(test_function)
         decorated_function()
-        
+
         # Assert
         mock_logger.info.assert_called_once_with("--- %s - start ---", "test_task")
         test_function.assert_called_once()
@@ -68,21 +67,21 @@ class TestTaskBase(BaseTestCase):
         # Setup
         test_exception = Exception("Test exception")
         mock_exc_info.return_value = (None, None, "traceback_object")
-        
+
         # Create a mock frame with a function attribute
         mock_frame = MagicMock()
         mock_frame.function = "test_function"
         mock_getinnerframes.return_value = [MagicMock(), mock_frame]
-        
+
         mock_format_log_message.return_value = "Formatted log message"
-        
+
         # Execute
         handle_periodic_task_exception(test_exception)
-        
+
         # Assert
         mock_exc_info.assert_has_calls([call(), call(), call(), call()])
         mock_getinnerframes.assert_called_once_with("traceback_object")
-        
+
         # Check that format_log_message was called with the correct parameters
         mock_format_log_message.assert_called_once_with(
             message="Error while executing periodic background task",
@@ -90,13 +89,10 @@ class TestTaskBase(BaseTestCase):
             exception=test_exception,
             username=None,
         )
-        
+
         # Check that the error was logged
-        mock_logger.error.assert_has_calls([
-            call("Formatted log message"),
-            call("NoneType: None\n")
-        ])
-        
+        mock_logger.error.assert_has_calls([call("Formatted log message"), call("NoneType: None\n")])
+
         # Check that send_task_exception_notification was called with the correct parameters
         mock_send_notification.assert_called_once_with(
             function="test_function", arguments=None, user=None, exception=test_exception, product=None
@@ -113,10 +109,10 @@ class TestTaskBase(BaseTestCase):
         test_exception = Exception("Test exception")
         mock_exc_info.return_value = (None, None, None)
         mock_format_log_message.return_value = "Formatted log message"
-        
+
         # Execute
         handle_periodic_task_exception(test_exception)
-        
+
         # Assert
         mock_exc_info.assert_has_calls([call(), call(), call()])
 
@@ -127,13 +123,10 @@ class TestTaskBase(BaseTestCase):
             exception=test_exception,
             username=None,
         )
-        
+
         # Check that the error was logged
-        mock_logger.error.assert_has_calls([
-            call("Formatted log message"),
-            call("NoneType: None\n")
-        ])
-        
+        mock_logger.error.assert_has_calls([call("Formatted log message"), call("NoneType: None\n")])
+
         # Check that send_task_exception_notification was called with the correct parameters
         mock_send_notification.assert_called_once_with(
             function=None, arguments=None, user=None, exception=test_exception, product=None
@@ -150,19 +143,19 @@ class TestTaskBase(BaseTestCase):
         # Setup
         test_exception = Exception("Test exception")
         mock_exc_info.return_value = (None, None, "traceback_object")
-        
+
         # Create a mock frame with only one frame (less than required)
         mock_getinnerframes.return_value = [MagicMock()]
-        
+
         mock_format_log_message.return_value = "Formatted log message"
-        
+
         # Execute
         handle_periodic_task_exception(test_exception)
-        
+
         # Assert
         mock_exc_info.assert_has_calls([call(), call(), call(), call()])
         mock_getinnerframes.assert_called_once_with("traceback_object")
-        
+
         # Check that format_log_message was called with the correct parameters
         mock_format_log_message.assert_called_once_with(
             message="Error while executing periodic background task",
@@ -170,13 +163,10 @@ class TestTaskBase(BaseTestCase):
             exception=test_exception,
             username=None,
         )
-        
+
         # Check that the error was logged
-        mock_logger.error.assert_has_calls([
-            call("Formatted log message"),
-            call("NoneType: None\n")
-        ])
-        
+        mock_logger.error.assert_has_calls([call("Formatted log message"), call("NoneType: None\n")])
+
         # Check that send_task_exception_notification was called with the correct parameters
         mock_send_notification.assert_called_once_with(
             function=None, arguments=None, user=None, exception=test_exception, product=None
